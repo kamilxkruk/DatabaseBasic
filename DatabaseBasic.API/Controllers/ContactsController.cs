d6de564a1b008498e6f34e7fd2a04704c3f42fe1
kamilxkruk@gmail.com <kamilxkruk@gmail.com>
2020-04-30 16:42:12 +0200

    Add WEB API

--- DatabaseBasic.API/Controllers/ContactsController.cs
+++ DatabaseBasic.API/Controllers/ContactsController.cs
@@ -14,10 +14,53 @@ namespace DatabaseBasic.API.Controllers
     public class ContactsController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<Contact> Get()
+        public ActionResult<List<Contact>> Get()
         {
             var result = new ContactDAL().GetContacts();
             return Ok(result);
         }
+
+        [HttpGet("{contactId}")]
+        public ActionResult<Contact> GetById(int contactId)
+        {
+            var result = new ContactDAL().GetContact(contactId);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public ActionResult<Contact> AddContact([FromBody] Contact contact)
+        {
+            var contactsDAL = new ContactDAL();
+            var result = contactsDAL.InsertContact(contact);
+            return Ok(result);
+        }
+
+        [HttpPut("{contactId}")]
+        public ActionResult<Contact> UpdateContact(int contactId, [FromBody] Contact contact)
+        {
+            var contactsDAL = new ContactDAL();
+            contact.Id = contactId;
+            var result = contactsDAL.UpdateContact(contact);
+            if (result == null)
+            {
+                return BadRequest();
+            }
+            return Ok(result);
+        }
+
+        [HttpDelete("{contactId}")]
+        public ActionResult<bool> DeleteContact(int contactId)
+        {
+            var contactsDAL = new ContactDAL();
+            var result = contactsDAL.DeleteContact(contactId);
+            return Ok(result);
+        }
+
+
+
     }
 }
